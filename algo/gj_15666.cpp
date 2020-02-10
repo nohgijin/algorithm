7e6b90002ab5e71a25d82c4ff7bb65327eb2b4e2
nohgijin <ynda11111@naver.com>
2020-02-10 18:48:54 +0900

    boj 15666

---
+++ algo/gj_15666.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#pragma warning( disable : 4996 )
+
+vector <int> arr;
+vector <int> r_stack;
+int N, M;
+
+void stackPrint() {
+	for (int k = 0; k < r_stack.size(); k++) {
+		printf("%d ", r_stack[k]);
+	}
+	printf("\n");
+}
+
+// r스택에 중복되지 않은 값을 넣음.
+void NM(int start, int depth) {
+	int NM_used[10001] = { 0, };
+
+	for (int j = start; j < N; j++) {
+		if (NM_used[arr[j]] == true) continue;
+
+		r_stack.push_back(arr[j]);
+		NM_used[arr[j]] = 1;
+
+		if (depth == M) stackPrint();
+		else NM(j, depth + 1);
+
+
+		r_stack.pop_back();
+	}
+}
+
+int main() {
+	scanf("%d %d", &N, &M);
+	for (int i = 1; i <= N; i++) {
+		int tmp;
+		scanf("%d", &tmp);
+		arr.push_back(tmp);
+	}
+	sort(arr.begin(), arr.end());
+	NM(0, 1);
+}
