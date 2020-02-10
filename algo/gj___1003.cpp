aa649282c138f5376cb623091cb1b4484ca73983
nohgijin <ynda11111@naver.com>
2020-02-10 18:47:42 +0900

    boj 1003

---
+++ algo/gj___1003.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <iostream>
+#pragma warning( disable : 4996 )
+int number[41][2] = { 0, };
+
+int main() {
+	int T, N;
+	number[0][0] = 1;
+	number[1][1] = 1;
+	scanf("%d", &T);
+	for (int k = 2; k <= 40; k++) {
+		number[k][0] = number[k - 1][0] + number[k - 2][0];
+		number[k][1] = number[k - 1][1] + number[k - 2][1];
+	}
+	for (int i = 1; i <= T; i++) {
+		scanf("%d", &N);
+		printf("%d %d", number[N][0], number[N][1]);
+		printf("\n");
+	}
+	return 0;
+}
