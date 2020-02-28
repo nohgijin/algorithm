a3272cf79c120a573e9ddf444663d02869156dca
nohgijin <ynda11111@naver.com>
2020-02-28 17:15:24 +0900

    boj 10972

---
+++ algo/gj_10972.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#pragma warning(disable:4996)
+
+int N;
+int cnt = 0;
+bool bfind = false;
+bool realfind = false;
+int chat;
+bool visited[10100] = { false, };
+vector<int>input;
+vector<int> s;
+
+void print() {
+	int len = s.size();
+	for (int i = 0; i < len; i++) printf("%d ", s[i]);
+}
+
+void sun(int depth) {
+	for (int i = 1; i <= N; i++) {
+		if (bfind) {
+			if (visited[i] == true) continue;
+			visited[i] = true;
+			s.push_back(i);
+			if (depth < N) sun(depth + 1);
+			else {
+				print();
+				realfind = true;
+				return;
+			}
+		}
+		else {
+			if (input[depth] != i) continue;
+			if (visited[i] == true) continue;
+			visited[i] = true;
+			s.push_back(i);
+			if (depth < N) sun(depth + 1);
+			else bfind = true;
+			visited[i] = false;
+			s.pop_back();
+		}
+	}
+}
+
+int main() {
+	scanf("%d", &N);
+	input.push_back(0);
+	for (int i = 1; i <= N; i++) {
+		int tmp;
+		scanf("%d", &tmp);
+		input.push_back(tmp);
+	}
+	sun(1);
+	if (!realfind) printf("-1");
+}
