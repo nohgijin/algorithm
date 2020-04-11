a442c632d727474e5261e0dd777982e4c7941920
nohgijin <ynda11111@naver.com>
2020-04-11 23:57:44 +0900

    boj 9663

---
+++ algo/gj_9663.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#pragma warning(disable:4996)
+
+int N;
+
+int main() {
+	scanf("%d", &N);
+}
