2145c79eb3a31935d97790add1f0fba44f26fdac
nohgijin <ynda11111@naver.com>
2020-05-15 18:04:17 +0900

    boj 15665

---
+++ algo/gj__15665.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <map>
+#pragma warning(disable:4996)
+using namespace std;
+
+int N, M;
+vector<int> input;
+bool global_visited[10010] = { false, };
+vector<int> v;
+
+void printvec() {
+	int vs = v.size();
+	for (int i = 0; i < vs; i++) {
+		printf("%d ", v[i]);
+	}
+	printf("\n");
+}
+
+void dfs(int start, int depth) {
+	bool local_visited[10010] = { false, };
+	v.push_back(input[start]);
+	if (depth == N) printvec();
+	else {
+		for (int i = 0; i < N; i++) {
+			if (local_visited[input[i]]) continue;
+			local_visited[input[i]] = true;
+			dfs(i, depth + 1);
+		}
+	}
+	v.pop_back();
+}
+
+int main() {
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++) {
+		int t;
+		scanf("%d", &t);
+		input.push_back(t);
+	}
+	sort(input.begin(), input.end());
+
+	bool local_visited[10010] = { false, };
+	for (int i = 0; i < N; i++) {
+		//2
+		if (local_visited[input[i]]) continue;
+		local_visited[input[i]] = true;
+		dfs(i, 1);
+	}
+}
